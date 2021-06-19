77e5608f840af8b5983d8b4d00fbe1bc492682fd
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-19 12:20:15 +0530

    Done with css

---
+++ src/components/TempApp.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./css/style.css";
+
+const TempApp = () => {
+
+    return (
+        <>
+        <div className="box">
+            <div className="inputData">
+                <input
+                type="search"
+                className="inputFeild"
+                defaultValue=""
+                onChange={(event) => {
+
+                }}
+                />
+            </div>
+        <div className="info">
+            <h2 className="location">
+                <i class="fas fa-street-view"></i>
+            </h2>
+            <h1 className="temp">
+                5.25Cel
+            </h1>
+            <h3 className="tempin_max"> Min : 5.25Cel | Max : 5.25Cel </h3>
+        </div>
+        <div className="wave -one"></div>
+        </div>
+
+        </>
+    )
+}
+
+export default TempApp;
