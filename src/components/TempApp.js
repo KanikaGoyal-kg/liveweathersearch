f5caef08f9aa0758cc9ab1bd815e2893adbfc0d4
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-20 22:02:01 +0530

    done with api

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -1,50 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./css/style.css";
 
 const TempApp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("mumbai");
 
-    const [city, setCity] = useState(null);
-    const [search, setSearch] = useState("mumbai");
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b14425a6554d189a2d7dc18a8e7d7263`;
+      const response = await fetch(url);
+      // console.log(response)
+      const resjson = await response.json();
+      // console.log(resjson)
+      setCity(resjson.main);
+    };
+    fetchApi();
+  }, [setSearch]);
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            className="inputFeild"
+            defaultValue=""
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
 
-    useEffect( () => {
-        const fetchApi = async () => {
+        {!city ? (
 
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b14425a6554d189a2d7dc18a8e7d7263`
-            const response = await fetch(url);
-            // console.log(response)
-            const resjson = await response.json();
-            // console.log(resjson)
-            setCity(resjson)
-        }
-        fetchApi();
-    },[])
-    return (
-        <>
-        <div className="box">
-            <div className="inputData">
-                <input
-                type="search"
-                className="inputFeild"
-                defaultValue=""
-                onChange={(event) => {
-
-                }}
-                />
+            <>
+            <h1>No data found</h1>
+          </>
+         
+        ) : (
+            <div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fas fa-street-view"></i>
+                {search}
+              </h2>
+              <h1>{city.temp}</h1>
+              <h3 className="tempin_max"> Min : 5.25Cel | Max : 5.25Cel </h3>
             </div>
-        <div className="info">
-            <h2 className="location">
-                <i className="fas fa-street-view"></i>{search}
-            </h2>
-            <h1 className="temp">
-                5.25Cel
-            </h1>
-            <h3 className="tempin_max"> Min : 5.25Cel | Max : 5.25Cel </h3>
-        </div>
-        <div className="wave -one"></div>
-        </div>
-
-        </>
-    )
-}
+            <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default TempApp;
