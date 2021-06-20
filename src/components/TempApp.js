bc18e78228b26882839c9649029b86a56a4e51ba
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-20 22:05:10 +0530

    done with api

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -23,6 +23,7 @@ const TempApp = () => {
           <input
             type="search"
             className="inputFeild"
+            value={search}
             defaultValue=""
             onChange={(event) => {
               setSearch(event.target.value);
@@ -30,27 +31,18 @@ const TempApp = () => {
           />
         </div>
 
-        {!city ? (
-
-            <>
-            <h1>No data found</h1>
-          </>
-         
-        ) : (
-            <div>
+        
             <div className="info">
               <h2 className="location">
                 <i className="fas fa-street-view"></i>
                 {search}
               </h2>
-              <h1>{city.temp}</h1>
+              <h1>{city}</h1>
               <h3 className="tempin_max"> Min : 5.25Cel | Max : 5.25Cel </h3>
             </div>
             <div className="wave -one"></div>
             <div className="wave -two"></div>
             <div className="wave -three"></div>
-          </div>
-        )}
       </div>
     </>
   );
