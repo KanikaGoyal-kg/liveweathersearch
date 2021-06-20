40f8575aa649db05868d5441c030eb390ade90ef
Kanika Goyal <goyalkanu111@gmail.com>
2021-06-20 21:22:11 +0530

    done with api

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./css/style.css";
 
 const TempApp = () => {
 
-    const [city, setCity] = useState("pune");
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("mumbai");
 
+    useEffect( () => {
+        const fetchApi = async () => {
+
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b14425a6554d189a2d7dc18a8e7d7263`
+            const response = await fetch(url);
+            // console.log(response)
+            const resjson = await response.json();
+            // console.log(resjson)
+            setCity(resjson)
+        }
+        fetchApi();
+    },[])
     return (
         <>
         <div className="box">
@@ -20,7 +33,7 @@ const TempApp = () => {
             </div>
         <div className="info">
             <h2 className="location">
-                <i class="fas fa-street-view"></i>{city}
+                <i className="fas fa-street-view"></i>{search}
             </h2>
             <h1 className="temp">
                 5.25Cel
